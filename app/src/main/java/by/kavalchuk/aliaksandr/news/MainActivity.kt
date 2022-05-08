5b2459805fb4b246b857bc99e1d8bbc66931c755
Aliaksandr Kavalchuk <avkavalchuk@gmail.com>
2022-05-08 12:14:14 +0300

    Git Init

---
+++ app/src/main/java/by/kavalchuk/aliaksandr/news/MainActivity.kt
@@ -0,0 +1,30 @@
+package by.kavalchuk.aliaksandr.news
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import by.kavalchuk.aliaksandr.news.ui.theme.NewsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NewsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
